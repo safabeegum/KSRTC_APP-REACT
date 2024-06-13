f2f202f9b5c5500a5dabb3c137997de150667e43
safabeegum <pasafabeegum@gmail.com>
2024-06-13 20:46:03 +0530

    Add completed

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Add = () => {
+    const[data,setData]=useState(
+        {
+            "name":"",
+            "route":"",
+            "busno":"",
+            "driver":"",
+        }
+    )
+
+    const inputHandler=(event) => {
+        setData({...data,[event.target.name]:event.target.value})
+    }
+
+    const readValue=() => {
+        console.log(data)
+        axios.post("",data).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Successfully Added")
+                } else {
+                    alert("Error")
+                }
+            }
+        ).catch().finally()
+    }
   return (
     <div>
         <div className="container">
@@ -12,26 +39,26 @@ const Add = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">BUS NAME</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">ROUTE</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='route' value={data.route} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">BUS NO</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='busno' value={data.busno} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">DRIVER NAME</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='driver' value={data.driver} onChange={inputHandler}/>
                         </div>
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">SUBMIT</button>
+                            <button className="btn btn-success" onClick={readValue}>SUBMIT</button>
                         </div>
 
                     </div>
