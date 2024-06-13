a5e0706704acf50c491a766655a17b88519ae8d9
safabeegum <pasafabeegum@gmail.com>
2024-06-13 22:32:54 +0530

    SignUp completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Add from './components/Add';
 import Search from './components/Search';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
+import View from './components/View';
 
 function App() {
   return (
@@ -12,9 +14,10 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>}/>
-        <Route path='/signup' element={<Add/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/add' element={<Add/>}/>
         <Route path='/search' element={<Search/>}/>
-        <Route path='/view' element={<Home/>}/>
+        <Route path='/view' element={<View/>}/>
         </Routes></BrowserRouter>
     </div>
   );
