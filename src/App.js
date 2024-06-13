319ba4f7e247ab1ca3f5c5ea777b91c616d161bc
safabeegum <pasafabeegum@gmail.com>
2024-06-13 20:49:13 +0530

    Search Branch created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Add from './components/Add';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/add' element={</>}/>
+        <Route path='/add' element={<Add/>}/>
         <Route path='/search' element={<Home/>}/>
         <Route path='/view' element={<Home/>}/>
         </Routes></BrowserRouter>
