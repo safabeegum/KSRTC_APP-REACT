27aa5416c6f0146dd6370dd92a76cd0396a7db29
safabeegum <pasafabeegum@gmail.com>
2024-06-13 20:22:11 +0530

    Home completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/add' element={</>}/>
+        <Route path='/search' element={<Home/>}/>
+        <Route path='/view' element={<Home/>}/>
+        </Routes></BrowserRouter>
     </div>
   );
 }
